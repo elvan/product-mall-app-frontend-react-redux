42299a94856e605827f4ec442a0abba5ee488494
Elvan Hidayat <elvanhd@gmail.com>
2023-06-12 03:23:27 +0700

    product details screen

--- src/index.js
+++ src/index.js
@@ -11,11 +11,13 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 );
