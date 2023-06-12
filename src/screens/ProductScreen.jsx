2131b8e48b4911b7cf503302fe2f52c4da1a1856
Elvan Hidayat <elvanhd@gmail.com>
2023-06-12 10:02:44 +0700

    fetching product details

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,11 +1,21 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
