b0798d069065510fe1d31aa6e4c364e56ff81879
Elvan Hidayat <elvanhd@gmail.com>
2023-06-14 00:03:24 +0700

    cart screen

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import {
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import store from './store';
@@ -20,6 +21,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
     </Route>
   )
 );
