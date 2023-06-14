0ee55a8086b142acf5258b44faf4a598f3094bab
Elvan Hidayat <elvanhd@gmail.com>
2023-06-15 01:43:42 +0700

    update users

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import reportWebVitals from './reportWebVitals';
 import OrderListScreen from './screens/admin/OrderListScreen';
 import ProductEditScreen from './screens/admin/ProductEditScreen';
 import ProductListScreen from './screens/admin/ProductListScreen';
+import UserEditScreen from './screens/admin/UserEditScreen';
 import UserListScreen from './screens/admin/UserListScreen';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
@@ -52,6 +53,7 @@ const router = createBrowserRouter(
         <Route path='/admin/productlist' element={<ProductListScreen />} />
         <Route path='/admin/userlist' element={<UserListScreen />} />
         <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
+        <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
       </Route>
     </Route>
   )
