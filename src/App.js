e8c086b9f237027dbbf03405749b5d789280e924
Elvan Hidayat <elvanhd@gmail.com>
2023-06-14 14:30:08 +0700

    token expiration time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { logout } from './slices/authSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const expirationTime = localStorage.getItem('expirationTime');
+    if (expirationTime) {
+      const currentTime = new Date().getTime();
+      if (currentTime > expirationTime) {
+        dispatch(logout());
+      }
+    }
+  }, [dispatch]);
+
   return (
     <>
       <ToastContainer />
