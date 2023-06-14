1b3c92ff8f9732a507bdbd62f48fafdb0a921414
Elvan Hidayat <elvanhd@gmail.com>
2023-06-14 14:19:04 +0700

    nav-dropdown alignment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,7 +47,11 @@ const Header = () => {
               </LinkContainer>
               {userInfo ? (
                 <>
-                  <NavDropdown title={userInfo.name} id='username'>
+                  <NavDropdown
+                    align={'end'}
+                    title={userInfo.name}
+                    id='username'
+                  >
                     <LinkContainer to='/profile'>
                       <NavDropdown.Item>Profile</NavDropdown.Item>
                     </LinkContainer>
